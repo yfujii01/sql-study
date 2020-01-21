acf9a2a97e7fbe4a5dd90c975800dd1eb3c363f7
yfujii01 <yfujii01@gmail.com>
2020-01-21 20:28:35 +0900

    init

---
+++ src/test/java/com/yfujii01/sqlstudy/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yfujii01.sqlstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
